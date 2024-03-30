20851da88cd198a1cde89647b661720426c8589a
Anatolii Khramchenko <71408259+xtor007@users.noreply.github.com>
2024-03-30 16:48:36 +0200

    Initial Commit

---
+++ MapView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapView
+//
+//  Created by Anatoliy Khramchenko on 30.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
